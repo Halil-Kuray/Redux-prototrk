dc2926bbcc258221e2708f50730d8793a11868c8
Halil-Kuray <71791007+Halil-Kuray@users.noreply.github.com>
2023-03-21 15:11:50 +0100

    post fetch

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,18 +1,59 @@
 import Test from "./components/Test";
-import { useState } from "react";
+import { useState, useEffect} from "react";
 
 
 function App() {
 
-  const [ show, setShow] = useState(false)
+  const [ show, setShow ] = useState(false)
+  const [ user, setUser ] = useState(false)
+
+  const addPost = (data) => {
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: 'POST',
+      body: JSON.stringify(data)
+      .then(res => res.json())
+      .then(data => console.log(data))
+      .catch(err => console.log(err))
+    })
+  }
+
+
+ useEffect(() => {
+
+  fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => {
+      if( res.ok && res.status == 200) {
+        return res.json()
+      }
+    })
+    .then(data => setUser(data))
+    .catch(err =>console.log(err))
+
+    addPost({
+      userId: 999,
+      title: 'Sample Post',
+      body: 'Post Content'
+    })
+ })
 
   return (
-    <div>
+    <main>
+    <section>
       <button onClick={() => setShow(prevState => !prevState)}>
         {show ? "Hide" : "Show"}
       </button>
       {show && <Test/>}
-    </div>
+    </section>
+
+    <ul>
+      { user && user.map((pUser) => (
+        <li key={pUser.id}>
+          {pUser.name}
+        </li>
+      ))}
+
+    </ul>
+    </main>
   );
 }
 
